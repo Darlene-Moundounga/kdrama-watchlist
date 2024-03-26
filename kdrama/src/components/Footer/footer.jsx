7b3443eea99a4e54904137541694230c30bf3dd1
Darlene-M <moundoungadarlene@gmail.com>
2024-03-26 10:33:30 +0100

    Add footer content

---
+++ kdrama/src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+export default function () {
+    return (
+        <footer id="footer">
+            <a href="https://www.tiktok.com/@i.am.nessaaaa"><i class="fa-brands fa-tiktok"></i></a>
+            <div>
+                <h3>Des suggestions d'améliorations ?</h3>
+                <p>Contactez-nous à cette adresse : dramaverse@gmail.com</p>
+            </div>
+            <p>Tous droits réservés <i class="fa-regular fa-copyright"></i></p>
+        </footer>
+    )
+}

4ccef4b3613b34e4c93aa4c4fd28b612cd7ee383
Darlene-M <moundoungadarlene@gmail.com>
2024-03-26 10:09:21 +0100

    Add icon for about section & style

--- kdrama/src/components/About/About.jsx
+++ kdrama/src/components/About/About.jsx
@@ -1,12 +1,12 @@
 function About() {
     return (
         <section className="about">
+            <img src="https://cdn-icons-png.flaticon.com/128/6740/6740284.png" alt="icone coeur coréen" />
             <p className="aboutText">Bienvenue sur <span>DramaVerse</span> , votre espace dédié aux passionnés de K-dramas !<br/> 
                 Plongez dans l'univers captivant des séries coréennes avec cette watchlist.<br/> 
-                Explorez les résumés, découvrez les acteurs talentueux, 
-                et créer votre propre watchlist K-Drama.<br/> 
-                De la romance envoûtante à l'action palpitante, 
-                <span>DramaVerse</span> est le site ultime pour vivre pleinement l'expérience des K-dramas. <br /> 
+                Explorez les résumés, découvrez les acteurs talentueux et choisissez le prochain k-drama que vous regarderez.<br/> 
+                <br />De la romance envoûtante à l'action palpitante,  
+                 <span> DramaVerse</span> est le site ultime pour vivre pleinement l'expérience des K-dramas. <br /> 
                 Rejoignez-nous et laissez-vous emporter dans le monde magique du drama coréen !
             </p>
         </section>
